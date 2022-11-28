c5fcc5e5296bc470db376f6ba2733d02b12981d0
JakubCzura <76125047+JakubCzura@users.noreply.github.com>
2022-11-29 00:24:01 +0100

    Add MainWindow and SavedQuantitieswindow

---
+++ Views/Windows/SavedInequalities.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace QuadraticInequalities.Views.Windows
+{
+    /// <summary>
+    /// Interaction logic for SavedInequalities.xaml
+    /// </summary>
+    public partial class SavedInequalities : Window
+    {
+        public SavedInequalities()
+        {
+            InitializeComponent();
+        }
+    }
+}
